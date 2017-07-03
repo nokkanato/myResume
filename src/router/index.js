faaf67823b852ecccb87c94324e02aeb0ec6f2b0
NOK <nokkanato@gmail.com>
2017-07-03 09:29:45 +0700

    make it better

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Main from '@/components/Main'
 import About from '@/components/About'
-import Skill from '@/components/Skill'
 
 Vue.use(Router)
 
@@ -10,18 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Main',
-      component: Main
-    },
-    {
-      path: '/About',
       name: 'About',
       component: About
-    },
-    {
-      path: '/Skill',
-      name: 'Skill',
-      component: Skill
     }
   ]
 })
